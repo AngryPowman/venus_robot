72f4d5213936d0c4ea3ec50f79af8c9081e2f8dc
AngryPowman <138001655@qq.com>
2013-09-24 00:02:06 +0800

    Add venus_robot.

---
+++ src/RobotWatchman/RobotWatchman/frmMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RobotWatchman
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Protocol.RobotLoginReq req = new Protocol.RobotLoginReq();
+            req.account = "138001655";
+
+            startNetwork();
+        }
+
+        private void addLog(string log)
+        {
+            lstLog.Items.Add("[" + DateTime.Now.ToString() + "] " + log);
+        }
+
+        TcpClient client = new TcpClient();
+        private void startNetwork()
+        {
+            addLog("CLIENT : socket 连接成功 ...");
+
+            using (NetworkStream stream = client.GetStream())
+            {
+                //发送
+                //Console.WriteLine("CLIENT : 发送数据 ...");
+
+                //msg.WriteTo(stream);
+
+                //关闭
+                stream.Close();
+            }
+            client.Close();
+            addLog("CLIENT : 关闭 ...");
+        }
+
+    }
+}
